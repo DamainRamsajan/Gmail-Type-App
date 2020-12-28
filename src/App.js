a83ad86c065ce260d9b5f17d0a7a1d3d7306f3e7
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-28 11:48:15 +0100

    Send Mail Component Complete

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Sidebar from './Sidebar';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Mail from "./Mail"
 import EmailList from "./EmailList"
+import SendMail from './SendMail';
 
 
 function App() {
   return (
     <Router>
-       <div className="App">
+       <div className="app">
         <Header />
         <div className = "app__body">
           <Sidebar />
@@ -23,7 +24,7 @@ function App() {
               </Route>
           </Switch>
         </div>
-        
+        <SendMail/>
       </div>
     </Router>
      
