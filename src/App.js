1ccede42fc0768852509ef64a2e6bd291ee21eec
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-29 00:55:16 +0100

    version one demo complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -6,16 +6,30 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Mail from "./Mail"
 import EmailList from "./EmailList"
 import SendMail from './SendMail';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
-import { selectUser } from './features/userSlice';
+import { login, selectUser } from './features/userSlice';
 import Login from './Login';
+import { auth } from './firebase';
 
 
 function App() {
 
   const setMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user){
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        }))
+      }
+    })
+  }, [])
 
   return (
     <Router>
